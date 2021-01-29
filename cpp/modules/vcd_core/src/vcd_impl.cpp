a995a52c636676c46303703d26cafd97847c81c7
jgoenetxea <jgoenetxea@gmail.com>
2021-01-29 14:29:52 +0100

    Included function to generate uuid-v4 instances

--- cpp/modules/vcd_core/src/vcd_impl.cpp
+++ cpp/modules/vcd_core/src/vcd_impl.cpp
@@ -14,12 +14,55 @@
 #include <regex>
 #include "vcd_impl.h"
 
+#include <openssl/rand.h>
+
 using std::string;
 
 namespace vcd {
 
 class CoordSys {
 };
+
+/** @brief Generate a Version 4 UUID according to RFC-4122
+ *
+ * Uses the openssl RAND_bytes function to generate a
+ * Version 4 UUID.
+ *
+ * @param buffer A buffer that is at least 38 bytes long.
+ * @retval 1 on success, 0 otherwise.
+ */
+std::string
+uuid_v4_gen() {
+    union {
+        struct {
+            uint32_t time_low;
+            uint16_t time_mid;
+            uint16_t time_hi_and_version;
+            uint8_t  clk_seq_hi_res;
+            uint8_t  clk_seq_low;
+            uint8_t  node[6];
+        };
+        uint8_t __rnd[16];
+    } uuid;
+
+    int rc = RAND_bytes(uuid.__rnd, sizeof(uuid));
+
+    // Refer Section 4.2 of RFC-4122
+    // https://tools.ietf.org/html/rfc4122#section-4.2
+    const auto cshr = (uuid.clk_seq_hi_res & 0x3F) | 0x80;
+    uuid.clk_seq_hi_res = static_cast<uint8_t>(cshr);
+    const auto thav = (uuid.time_hi_and_version & 0x0FFF) | 0x4000;
+    uuid.time_hi_and_version = static_cast<uint16_t>(thav);
+
+    char uuidv4[38];
+    snprintf(uuidv4, 38, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+            uuid.time_low, uuid.time_mid, uuid.time_hi_and_version,
+            uuid.clk_seq_hi_res, uuid.clk_seq_low,
+            uuid.node[0], uuid.node[1], uuid.node[2],
+            uuid.node[3], uuid.node[4], uuid.node[5]);
+
+    return std::string(uuidv4);
+}
 
 ///////////////////////////////////////////////
 // VCD_Impl
