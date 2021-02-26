73641f42244a67fa77c4ae1f05039c0e59a922e3
jgoenetxea <jgoenetxea@gmail.com>
2021-02-26 11:56:11 +0100

    Included test case for uuids

---
+++ cpp/test/vcd_core/test_uuid/src/test_uuid.cpp
@@ -0,0 +1,189 @@
+/*
+* VCD (Video Content Description) library v4.3.0
+*
+* Project website: http://vcd.vicomtech.org
+*
+* Copyright (C) 2020, Vicomtech (http://www.vicomtech.es/),
+* (Spain) all rights reserved.
+
+* VCD is a C++ library to create and manage VCD content version 4.3.0.
+* VCD is distributed under MIT License. See LICENSE.
+*
+*/
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// -- Project -- //
+#include <fstream>
+#if defined(__linux__)
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #include <filesystem>
+    #ifdef _MSC_VER == 1900
+    namespace fs = std::experimental::filesystem;
+    #else
+    namespace fs = std::filesystem;
+    #endif
+#endif
+
+#include <nlohmann/json.hpp>
+
+#include "vcd.h"
+#include "vcd_types.h"
+#include "setup_strings.h"
+#include "test_utils.h"
+
+#include "vcd_impl.h"
+
+static char asset_path[] = TEST_ASSET_FOLDER;
+
+using vcd::VCD;
+using vcd::VCD_ptr;
+using vcd::types::Bbox;
+using vcd::types::Vec;
+using vcd::types::Num;
+using vcd::types::Boolean;
+
+using std::string;
+
+using nlohmann::json;
+
+using vcd::obj_uid;
+
+// Indirection class to get inner elements of vcd instance
+namespace vcd {
+class VCD_Inner : public vcd::VCD_Impl {
+ public:
+    VCD_Inner() : vcd::VCD_Impl("") {
+    }
+
+    json*
+    get_inner_element(const vcd::ElementType type, const std::string &uid) {
+        return get_element(type, vcd::UID(uid));
+    }
+};
+}  // namespace vcd
+
+// ----- //
+
+std::string
+getStreamAsString(const std::istream& in) {
+    std::stringstream out;
+    out << in.rdbuf();
+    return out.str();
+}
+
+std::string
+getFileAsString(const std::string& filePath) {
+    std::ifstream stream;
+    try {
+        // Set to throw on failure
+        stream.exceptions(std::fstream::failbit | std::fstream::badbit);
+        stream.open(filePath);
+    } catch (std::system_error& error) {
+        std::cerr << "Failed to open '" << filePath
+                  << "'\n" << error.code().message() << std::endl;
+        return "Open fail";
+    }
+
+    return getStreamAsString(stream);
+}
+
+SCENARIO("Create some basic content, without time information") {
+    GIVEN("A VCD structure") {
+        THEN("Test different uid types") {
+            // 1.- Create a VCD instance
+            vcd::VCD_Inner vcd_impl;
+//            VCD_ptr vcd = VCD::create();
+
+            // We can add elements and get UIDs as strings
+            vcd::obj_args mike_args;
+            mike_args.semantic_type = "Person";
+            const auto uid0 = vcd_impl.add_object("Mike", mike_args);
+            REQUIRE(uid0 == "0");
+
+            // We can also specify which UID we will like our elements to have
+            // We can only use strings
+            // Response is always string
+            vcd::obj_args susan_args;
+            susan_args.semantic_type = "Person";
+            susan_args.uid = "2";
+            const obj_uid uid2 = vcd_impl.add_object("Susan", susan_args);
+            REQUIRE(uid2 == "2");
+
+            // The user must use uuids in string format for all public functions
+            vcd_impl.add_object_data(uid2, Boolean("checked", true));
+            vcd_impl.add_object_data("2", Boolean("double-checked", true));
+
+            // Same happens with ontology uids
+            const vcd::ont_uid ont_uid_0 = vcd_impl.add_ontology(
+                                    "http://www.vicomtech.org/viulib/ontology");
+            REQUIRE(!ont_uid_0.empty());
+
+            // Test ontology uid
+            vcd::obj_args mark_args;
+            mark_args.semantic_type = "#Pedestrian";
+            mark_args.ontology_uid = ont_uid_0;
+            const obj_uid uid3 = vcd_impl.add_object("Mark", mark_args);
+            const json* data = vcd_impl.get_inner_element(vcd::object, uid3);
+            REQUIRE(data != nullptr);
+            REQUIRE((*data)["ontology_uid"] == "0");
+
+            // Generate json data
+            const bool pretty = true;
+            const std::string vcd_out_pretty = vcd_impl.stringify(pretty);
+
+            // All returned UIDs are strings, and when written into JSON as well
+            // print(vcd.stringify(False))
+            const std::string ref_json =
+                    "{"
+                    "\"vcd\":{"
+                    "   \"metadata\":{"
+                    "       \"schema_version\":\"4.3.0\""
+                    "   },"
+                    "   \"objects\":{"
+                    "       \"0\":{"
+                    "           \"name\":\"Mike\","
+                    "           \"type\":\"Person\""
+                    "       },"
+                    "       \"2\":{"
+                    "           \"name\":\"Susan\","
+                    "           \"type\":\"Person\","
+                    "           \"object_data\":{"
+                    "               \"boolean\":[{"
+                    "                   \"name\":\"checked\","
+                    "                   \"val\":true"
+                    "                },{"
+                    "                   \"name\":\"double-checked\","
+                    "                   \"val\":true"
+                    "                }]"
+                    "           },"
+                    "           \"object_data_pointers\":{"
+                    "               \"checked\":{"
+                    "                   \"type\":\"boolean\","
+                    "                   \"frame_intervals\":[]"
+                    "               },"
+                    "               \"double-checked\":{"
+                    "                   \"type\":\"boolean\","
+                    "                   \"frame_intervals\":[]"
+                    "               }"
+                    "           }"
+                    "       },"
+                    "       \"3\":{"
+                    "           \"name\":\"Mark\","
+                    "           \"type\":\"#Pedestrian\","
+                    "           \"ontology_uid\":\"0\""
+                    "       }"
+                    "   },"
+                    "   \"ontologies\":{"
+                    "       \"0\":\"http://www.vicomtech.org/viulib/ontology\""
+                    "   }"
+                    "   }"
+                    "}";
+            const json ref_data = json::parse(ref_json);
+            const json test_data = json::parse(vcd_out_pretty);
+            REQUIRE(check_json_level(test_data, ref_data));
+        }
+    }
+}
