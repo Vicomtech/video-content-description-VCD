c88165f88dcdda9cc27fc6ef3e2c1bfc5324fbd2
jgoenetxea <jgoenetxea@gmail.com>
2021-06-04 09:27:56 +0200

    Included a sample to show how to include installed VCD in an external project

---
+++ cpp/samples/sample_include_in_external_proj/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+#include "vcd/vcd.h"
+
+int main() {
+    // Load VCD library and generate a sequence of N bboxes
+    const size_t N = 10000;
+
+    // Create vcd instance
+    vcd::VCD_ptr vcd = vcd::VCD::create();
+
+    // Define an object to be tracked during the sequence (a static bbox)
+    vcd::element_args obj_args;
+    obj_args.semantic_type = "static_object";
+
+    // Add the object to the current vcd definition
+    std::string uid_obj = vcd->add_object("the_obj", obj_args);
+
+    // Generate a loop and include a set of bboxes as frames
+    const int& rect_w = 50;
+    const int& rect_h = 50;
+    const int center_x = 400;
+    const int center_y = 300;
+    for (size_t frame_cont = 0; frame_cont < N; ++frame_cont) {
+        vcd->add_bbox_to_object(uid_obj, "obj_bbox",
+                                {center_x, center_y, rect_w, rect_h},
+                                frame_cont);
+    }
+
+    // Export the generated definition to an external file
+    const bool pretty = true;
+    vcd->save("sample_include_int_external_proj.vcd", pretty);
+}
