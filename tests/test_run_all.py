97de588d95cd048b2af4b61e39f1c16311b1ab9e
Marcos Nieto <marcos.nieto.doncel@gmail.com>
2021-06-17 18:40:51 +0200

    Clean up test_run_all script

--- tests/test_run_all.py
+++ tests/test_run_all.py
@@ -11,13 +11,10 @@ VCD is distributed under MIT License. See LICENSE.
 
 """
 
-
-#import os
 import subprocess
 
 # Clean existing json or txt files at etc
 dir_name = "./etc/"
-#test = os.listdir(dir_name)
 
 subprocess.check_call(["python.exe", "test_basic.py"])
 subprocess.check_call(["python.exe", "test_converters.py"])
@@ -33,17 +30,3 @@ subprocess.check_call(["python.exe", "test_openlabel_tagging.py"])
 subprocess.check_call(["python.exe", "test_uuid.py"])
 subprocess.check_call(["python.exe", "test_bbox.py"])
 
-'''
-os.system("python test_basic.py && "
-          "python test_converters.py && "
-          #"python test_sanity.py && "
-          #"python test_serializer.py &&"
-          "python test_mesh.py &&"
-          "python test_image.py &&"
-          "python test_stream_frame_properties.py &&"
-          "python test_action_properties.py &&"
-          "python test_semantics.py &&"
-          "python test_modify.py &&"
-          "python test_geometries.py &&"
-          "python test_openlabel_labeling.py")
-'''
