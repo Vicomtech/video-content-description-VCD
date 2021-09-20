21c2ed9f73d051ad36611a4d142758efd248434d
Marcos Nieto <marcos.nieto.doncel@gmail.com>
2021-09-20 18:50:44 +0200

    Upgrade setup.py to read Unicode characters at README.md

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read().encode("UTF-8")
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="vcd",
